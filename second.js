87fc73f8504ff412352e40fedd4827eff558b79d
Diana Agata Leonte <dyaneddd@yahoo.com>
2018-02-24 16:58:22 +0200

    code review

--- second.js
+++ second.js
@@ -5,8 +5,7 @@ class View {
   }
 
   getUrlParam(name) {
-
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    let results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     if (results == null) {
       return null;
     } else {
@@ -15,37 +14,45 @@ class View {
   };
 
   showPostAndComments() {
-    var post = this.model.getPostById(this.id);
-    // console.log(post);
-
+    let post = this.model.getPostById(this.id);
     post.then(res => {
-      var postContent = document.getElementById('post');
-      var title = document.createElement('h2');
-      var content = document.createElement('p');
-      title.innerHTML = res.title;
-      content.innerHTML = res.body;
-      postContent.appendChild(title);
-      postContent.appendChild(content);
-      var comments = this.model.getCommentsForPost(res.id);
+      let postHtml = this.buildHtmlForPost(res);
+      let comments = this.model.getCommentsForPost(res.id);
       comments.then(result => {
-        console.log(result);
-        var commentList = document.getElementById('comments');
-        result.forEach(res => {
-          var comment = document.createElement('li');
-          var name = document.createElement('div');
-          var email = document.createElement('div');
-          var commentBody = document.createElement('div');
-
-          name.innerHTML = 'Name :  ' + res.name;
-          email.innerHTML = 'Email :  ' + res.email;
-          commentBody.innerHTML = 'Comment :  ' + res.body;
-          comment.appendChild(name);
-          comment.appendChild(email);
-          comment.appendChild(commentBody);
-          commentList.appendChild(comment);
-        });
+        result.forEach(res => this.buildHtmlForComments(res));
       });
     });
+  };
+
+  buildHtmlForPost(res) {
+    let postContent = document.getElementById('post');
+    let title = document.createElement('h2');
+    let content = document.createElement('p');
+
+    title.innerHTML = res.title;
+    content.innerHTML = res.body;
+    postContent.appendChild(title);
+    postContent.appendChild(content);
+
+    return postContent;
+  };
+
+  buildHtmlForComments(res) {
+    let commentList = document.getElementById('comments');
+    let comment = document.createElement('li');
+    let name = document.createElement('div');
+    let email = document.createElement('div');
+    let commentBody = document.createElement('div');
+
+    name.innerHTML = 'Name :  ' + res.name;
+    email.innerHTML = 'Email :  ' + res.email;
+    commentBody.innerHTML = 'Comment :  ' + res.body;
+    comment.appendChild(name);
+    comment.appendChild(email);
+    comment.appendChild(commentBody);
+    commentList.appendChild(comment);
+
+    return commentList;
   };
 };
 
